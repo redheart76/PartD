679ce2812396dfb3b0a9e10ea039b520a18f3b27
redheart76 <redheart76@gmail.com>
2020-09-16 22:52:26 +1200

    The third line is added.

--- BIT504_Ass2_PartD/src/Main.java
+++ BIT504_Ass2_PartD/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Sam");
 		System.out.println("My favourite color is black");
+		System.out.println("My favourite mode of transportation is train");
 	}
 
 }
